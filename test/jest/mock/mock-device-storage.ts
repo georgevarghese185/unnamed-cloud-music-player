9bb20947bb18a87e6a7a68230cecfd5160289dac
George Varghese M <georgevarghese185@users.noreply.github.com>
2022-01-08 17:20:41 +0530

    Reuse existing DeviceStorage and TrackStore implementations for tests instead of reimplementing mocks
    As the complexity of tests increase, more mock implementations will need to be added to MockDeviceStorage and MockTrackStore
    
    Instead of re-inventing the wheel, use the existing testable implementations of DeviceStorage (electron/node) and TrackStore (IndexedDb) for unit tests so that the mocks can be as low effort as possible

--- test/jest/mock/mock-device-storage.ts
+++ test/jest/mock/mock-device-storage.ts
@@ -1,52 +1,25 @@
-import {
-  DeviceFile,
-  DeviceStorage,
-  Directory,
-} from 'app/src-core/storage/device';
+import { NodeFsDeviceStorage } from 'app/src-electron/storage/device/node-fs-device-storage';
 import MemoryFileSystem from 'memory-fs';
-import { basename, extname } from 'path';
+import { dirname } from 'path';
 
-type StatType = ReturnType<InstanceType<typeof MemoryFileSystem>['statSync']>;
+export class MockDeviceStorage extends NodeFsDeviceStorage {
+  memoryFs: MemoryFileSystem;
 
-const toDeviceFile = (filePath: string, isDir: boolean): DeviceFile => {
-  const path = filePath;
-  const name = basename(filePath);
-
-  return isDir
-    ? ({ path, name, isDir: true } as Directory)
-    : { path, name, ext: extname(filePath), isDir: false };
-};
-
-export class MockDeviceStorage implements DeviceStorage {
-  public fs = new MemoryFileSystem();
-
-  async listFiles(dir: Directory) {
-    const files: string[] = await this.readdir(dir.path);
-    return Promise.all(
-      files.map(async (file) => {
-        const path = `${dir.path}/${file}`;
-        return toDeviceFile(path, await this.isDir(path));
-      }),
-    );
+  constructor() {
+    const memoryFs = new MemoryFileSystem();
+    super({
+      readdir: memoryFs.readdir.bind(memoryFs),
+      stat: memoryFs.stat.bind(memoryFs),
+    });
+    this.memoryFs = memoryFs;
   }
 
-  async getFile(path: string) {
-    return toDeviceFile(path, await this.isDir(path));
+  writeFile(path: string, contents: Buffer | string) {
+    this.createDir(dirname(path));
+    this.memoryFs.writeFileSync(path, contents);
   }
 
-  private async isDir(path: string): Promise<boolean> {
-    return (await this.stat(path)).isDirectory();
-  }
-
-  private async stat(path: string): Promise<StatType> {
-    return new Promise((resolve, reject) =>
-      this.fs.stat(path, (err, stat) => (err ? reject(err) : resolve(stat))),
-    );
-  }
-
-  private async readdir(path: string): Promise<string[]> {
-    return new Promise((resolve, reject) =>
-      this.fs.readdir(path, (err, stat) => (err ? reject(err) : resolve(stat))),
-    );
+  createDir(path: string) {
+    this.memoryFs.mkdirpSync(path);
   }
 }
