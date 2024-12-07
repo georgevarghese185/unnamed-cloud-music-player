ad80db26913334c5a7fbddc3485d4a931bbb0429
George Varghese M <georgevarghese185@users.noreply.github.com>
2024-12-07 16:01:27 -0500

    Add src-electron

---
+++ src-electron/storage/device/electron-device-storage.ts
@@ -0,0 +1,11 @@
+import type { DeviceFile, DeviceStorage, Directory } from 'app/src-core/storage/device'
+
+export class ElectronDeviceStorage implements DeviceStorage {
+  async listFiles(dir: Directory): Promise<DeviceFile[]> {
+    return window.bridge.file.listFiles(dir)
+  }
+
+  async getFile(path: string): Promise<DeviceFile> {
+    return window.bridge.file.getFile(path)
+  }
+}
